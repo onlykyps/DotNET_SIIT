ea7d1fd403b8090b535fd9304444013f219cf957
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-07 19:48:45 +0300

    operatori c#

---
+++ FirstApp/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+
+         int aa = 2;
+         int bb = 3;
+
+         float cc = aa * bb / (5f / 6f);
+         
+         Console.WriteLine("answer is " + cc);
+
+         int incr = 9;
+         //  inainte de trimitere a variabilei la tiparire
+         Console.WriteLine("primu e " + ++incr);
+         incr = 9;
+         //  dupa trimiterea a variabilei la tiparire
+         //  este o usoara diferenta de prioritare
+         Console.WriteLine("al doilea e " + incr++);
+
+         // la fel cu --decr si decr--
+
+         // daca e weekend si nu am obligatii dorm pana la pranz
+         // daca sunt promotii sau oferte ma duc la cumparaturi
+         // daca nu e weekend ma trezesc la 4
+      }
+   }
+}
