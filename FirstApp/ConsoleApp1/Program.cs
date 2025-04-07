979c93d08ce992e1530f532bb2a0ffefaeb6928f
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-07 20:14:06 +0300

    operatori c#
    bitwise

--- FirstApp/ConsoleApp1/Program.cs
+++ FirstApp/ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
          int bb = 3;
 
          float cc = aa * bb / (5f / 6f);
-         
+
          Console.WriteLine("answer is " + cc);
 
          int incr = 9;
@@ -28,6 +28,16 @@ namespace ConsoleApp1
          // daca e weekend si nu am obligatii dorm pana la pranz
          // daca sunt promotii sau oferte ma duc la cumparaturi
          // daca nu e weekend ma trezesc la 4
+
+         ushort exp1 = 0x00AB;
+         ushort exp2 = 0x00B1;
+         Console.WriteLine("exp1 & exp2 = {0:X}", exp1 & exp2);
+         Console.WriteLine("exp1 | exp2 = {0:X}", exp1 | exp2);
+         Console.WriteLine("~exp1 = {0:X}", ~exp1); // because of integral promotion
+
+         Console.WriteLine("exp1 ^ exp2 = {0:X}", exp1 ^ exp2);
+
+
       }
    }
 }
