b486a30ac385d943a9df645da7574c85adc13676
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-12 12:49:37 +0300

    dependencies
     <TargetFramework>net8.0</TargetFramework>

--- BlazorServerBudgetApp/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ BlazorServerBudgetApp/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -6,86 +6,80 @@
 
 namespace BlazorServerBudgetApp.Shared
 {
-    #line hidden
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Components;
+    #line default
+    using global::System;
+    using global::System.Collections.Generic;
+    using global::System.Linq;
+    using global::System.Threading.Tasks;
+    using global::Microsoft.AspNetCore.Components;
 #nullable restore
 #line 1 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using System.Net.Http;
+using System.Net.Http
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 2 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 3 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Authorization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 4 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Forms
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 5 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Routing
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 6 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 7 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.Web.Virtualization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 8 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.JSInterop;
+using Microsoft.JSInterop
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 9 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using BlazorServerBudgetApp;
+using BlazorServerBudgetApp
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 10 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using BlazorServerBudgetApp.Shared;
+using BlazorServerBudgetApp.Shared
 
-#line default
-#line hidden
 #nullable disable
-    public partial class SurveyPrompt : Microsoft.AspNetCore.Components.ComponentBase
+    ;
+    #line default
+    #line hidden
+    #nullable restore
+    public partial class SurveyPrompt : global::Microsoft.AspNetCore.Components.ComponentBase
+    #nullable disable
     {
         #pragma warning disable 1998
-        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
@@ -99,6 +93,7 @@ using BlazorServerBudgetApp.Shared;
 #line default
 #line hidden
 #nullable disable
+
     }
 }
 #pragma warning restore 1591
--- BlazorServerBudgetApp/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ BlazorServerBudgetApp/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -6,83 +6,77 @@
 
 namespace BlazorServerBudgetApp
 {
-    #line hidden
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Components;
+    #line default
+    using global::System;
+    using global::System.Collections.Generic;
+    using global::System.Linq;
+    using global::System.Threading.Tasks;
+    using global::Microsoft.AspNetCore.Components;
 #nullable restore
 #line 1 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using System.Net.Http;
+ using System.Net.Http
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 2 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Authorization;
+ using Microsoft.AspNetCore.Authorization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 3 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Authorization;
+ using Microsoft.AspNetCore.Components.Authorization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 4 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Forms;
+ using Microsoft.AspNetCore.Components.Forms
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 5 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Routing;
+ using Microsoft.AspNetCore.Components.Routing
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 6 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web;
+ using Microsoft.AspNetCore.Components.Web
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 7 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.AspNetCore.Components.Web.Virtualization;
+ using Microsoft.AspNetCore.Components.Web.Virtualization
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 8 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using Microsoft.JSInterop;
+ using Microsoft.JSInterop
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 9 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using BlazorServerBudgetApp;
+ using BlazorServerBudgetApp
 
-#line default
-#line hidden
 #nullable disable
+    ;
 #nullable restore
 #line 10 "D:\Dropbox\Scoala Informala de IT\DotNET\DotNET_SIIT\BlazorServerBudgetApp\_Imports.razor"
-using BlazorServerBudgetApp.Shared;
+ using BlazorServerBudgetApp.Shared
 
-#line default
-#line hidden
 #nullable disable
-    public partial class _Imports : System.Object
+    ;
+    #line default
+    #line hidden
+    #nullable restore
+    public partial class _Imports : object
+    #nullable disable
     {
         #pragma warning disable 1998
         protected void Execute()
