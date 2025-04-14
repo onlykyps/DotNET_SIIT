41893793fd8faf1e27bd3246b89bce1c25e8c68d
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-14 21:02:12 +0300

    exercitiu cu sirul lui Fibonacci

--- FirstApp/ConsoleApp1/Program.cs
+++ FirstApp/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Formats.Asn1;
 using System.Reflection;
 
 namespace ConsoleApp1
@@ -7,30 +8,63 @@ namespace ConsoleApp1
    {
       static void Main(string[] args)
       {
-         ushort input = ushort.Parse(Console.ReadLine());
-         bool ePrim = false;
+         //ushort input = ushort.Parse(Console.ReadLine());
+         //bool ePrim = false;
 
-         for (ushort i = 2; i <= input / 2; i++) 
+         //for (ushort i = 2; i <= input / 2; i++) 
+         //{
+         //   if(input % i == 0)
+         //   {
+         //      ePrim = false;
+         //      break;
+         //   }
+
+         //   //if (input % i == 0)
+         //   //{
+         //   //   break;
+         //   //   Console.WriteLine("numarul nu este prim");
+         //   //}
+         //   //else
+         //   //{
+         //   //   Console.WriteLine("numarul este prim");
+         //   //}
+         //}
+
+         //if (!ePrim) Console.WriteLine("{0} nu e prim", input);
+         //else Console.WriteLine("{0} e prim", input);
+
+         //gresit, de revizuit
+         //ushort input = ushort.Parse(Console.ReadLine());
+         //int fib0 = 1;
+         //int fib1 = 1;
+
+         //while(input > fib1)
+         //{
+         //   int temp = fib1;
+         //   fib1 = fib0 + fib1;
+         //   fib0 = temp;
+
+         //}
+
+         //Console.WriteLine(fib1);
+
+         uint a = 1; 
+         uint b = 1; 
+         uint c;
+         uint nrIntr;
+
+         Console.WriteLine("introduceti numar");
+         nrIntr = uint.Parse(Console.ReadLine());
+
+         do
          {
-            if(input % i == 0)
-            {
-               ePrim = false;
-               break;
-            }
-
-            //if (input % i == 0)
-            //{
-            //   break;
-            //   Console.WriteLine("numarul nu este prim");
-            //}
-            //else
-            //{
-            //   Console.WriteLine("numarul este prim");
-            //}
+            c = a + b;
+            a = b;
+            b = c;
          }
+         while (c <= nrIntr);
 
-         if (!ePrim) Console.WriteLine("{0} nu e prim", input);
-         else Console.WriteLine("{0} e prim", input);
+         Console.WriteLine("nr din sirul lui Fibonnaci cel mai aproape de {0} este {1}", nrIntr, (c == nrIntr ? c : a));
 
       }
    }
--- FirstApp/ConsoleApp1/obj/Debug/net5.0/ConsoleApp1.AssemblyInfo.cs
+++ FirstApp/ConsoleApp1/obj/Debug/net5.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b851ab2985d589090dea80941cc1b2cd48149fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d775e1ec3b24a6eb5fa6838f7d8af050eeef4552")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
