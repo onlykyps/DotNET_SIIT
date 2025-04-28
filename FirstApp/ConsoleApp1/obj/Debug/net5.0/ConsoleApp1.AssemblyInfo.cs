e13da9ebb1ccab2bf92030af2bb7885cca20b5e2
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-28 19:28:50 +0300

    c

--- FirstApp/ConsoleApp1/obj/Debug/net5.0/ConsoleApp1.AssemblyInfo.cs
+++ FirstApp/ConsoleApp1/obj/Debug/net5.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779823ccc729961f2a31d170d8315e6070e77b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70342ab0bcda6daab70a7901643bbcc9176fc058")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
