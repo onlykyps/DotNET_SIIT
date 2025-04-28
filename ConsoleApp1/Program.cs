b3906866c132dbd987467b7f85440673396aac74
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-04-28 19:42:49 +0300

    proprietati clase

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-   internal class Program
+   public class Program
    {
       static void Main(string[] args)
       {
+         Punct p1 = new Punct();
+         p1._x = 10;
+         p1._y = 5;
+
+         Punct p2 = new Punct
+         {
+            _x = 7,
+            _y = 9
+         };
+        
       }
    }
 }
--- ConsoleApp1/Punct.cs
+++ ConsoleApp1/Punct.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-   internal class Punct
+   public class Punct
    {
 
-   }
+		public Double _x, _y;
+
+		//private Double _coordX;
+		//private Double _coordY;
+
+
+		//public Double CoordX
+		//{
+		//	get { return _coordX; }
+		//	set { _coordX = value; }
+		//}
+
+		//public Double CoordY
+		//{
+		//	get { return _coordY; }
+		//	set { _coordY = value; }
+		//}
+
+	}
 }
