e0bd42f8d792ba1dc4efcfda1a95d4ce1f306b12
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-06-04 19:09:32 +0300

    exemplu exceptii

--- Exceptii/Program.cs
+++ Exceptii/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,73 @@ namespace Exceptii
    {
       static void Main(string[] args)
       {
+         bool firstNumberRead = false;
+
+
          Console.WriteLine("Introduceti doua numere de la tastatura");
-         Console.WriteLine("Primul numar");
-         int num1 = Convert.ToInt32(Console.ReadLine());
-         Console.WriteLine("Al doilea numar");
-         int num2 = Convert.ToInt32(Console.ReadLine());
+         int num1 = 0;
+         int num2 = 0;
 
+         while (true) 
+         {
+            try
+            {
+               if (!firstNumberRead)
+               {
+                  Console.WriteLine("Primul numar");
+                  num1 = Convert.ToInt32(Console.ReadLine());
+                  firstNumberRead = true;
+               }
+               Console.WriteLine("Al doilea numar");
+               num2 = Convert.ToInt32(Console.ReadLine());
+               int result = num1 / num2;
+               Console.WriteLine($"resultatul este ", result);
+               break;
+            }
+            catch (DivideByZeroException divZeroEx)
+            {
+               Console.WriteLine("Nu putem diviza cu 0");
+            }
+            catch (FormatException ex) 
+            { 
+               Console.WriteLine("input invalid. introduceti un integer");
+            }
+         }
+         
 
+         //idee gresita
+         //try
+         //{
+            
 
+            
+         //}
+         //catch (FormatException fEx)
+         //{
+         //   Console.WriteLine("Ati introdus un integer invalid: " + fEx.Message);
+
+         //   throw new ArgumentException("integer invalid", fEx);
+         //}
+        
+        
+
+         //try
+         //{
+         //   Console.WriteLine($"num1/num2={0}", CalculeazaImpartirea(num1, num2));
+         //}
+         //catch (DivideByZeroException divZeroEx)
+         //{
+         //   Console.WriteLine("Ati introdus num2 egal cu 0: " + divZeroEx.Message);
+         //   throw;
+         //}
+
+        
       }
+
+
+      //private static object CalculeazaImpartirea(int num1, int num2)
+      //{
+      //   return num1 / num2;
+      //}
    }
 }
