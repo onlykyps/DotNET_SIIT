8c5977e7d068779cea3fbc2bdfd6b3dfae1dd9a3
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-06-04 18:40:35 +0300

    exercitiu exceptii
    si ceva alt commit

---
+++ Exceptii/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptii
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Introduceti doua numere de la tastatura");
+         Console.WriteLine("Primul numar");
+         int num1 = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Al doilea numar");
+         int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+      }
+   }
+}
