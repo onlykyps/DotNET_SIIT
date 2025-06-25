780531176abeb616683f4095bc4b48de577491ff
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-06-25 20:40:08 +0300

    delegates

--- DelegatesAnon/Program.cs
+++ DelegatesAnon/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace DelegatesAnon
 {
+   public delegate void GreetDelegate(string name);
+
    public class Program
    {
+      static void SayHello(string n)
+      {
+         Console.WriteLine($"Hello {n}!");
+      }
+
+      static void SayGoodbye(string n)
+      {
+         Console.WriteLine($"Goodbye {n}!");
+      }
+
       static void Main(string[] args)
       {
+         GreetDelegate greetDelegate = SayHello;
+
+         greetDelegate("Ovidiu");
+
+         greetDelegate = SayGoodbye;
+
+         greetDelegate("Claudiu");
+
+         greetDelegate += SayHello;
+
+         greetDelegate("Ovidiu");
+
       }
    }
 }
