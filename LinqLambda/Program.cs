7eba1d99a5ef5e947df437156dfd6d0ff5b90f3e
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-07-08 19:04:17 +0300

    c

--- LinqLambda/Program.cs
+++ LinqLambda/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LinqLambda
 {
@@ -10,6 +12,25 @@ namespace LinqLambda
    {
       static void Main(string[] args)
       {
+         Hashtable ht = new Hashtable();
+         int index = 99;
+         Random rnd = new Random();
+         while (index >= 0)
+         {
+            ht.Add(index, rnd.Next(1000));
+            index--;
+         }
+
+         var keys = ht.OfType<DictionaryEntry>().
+                                     Where(entry => (int)entry.Value > 500)
+                                     .Select(entry => (entry.Key, entry.Value));
+         foreach (var pair in keys)
+         {
+            Console.WriteLine(pair);
+         }
+
+         XDocument xDocument = XDocument.Load("Customers.xml");
+
 
 
       }
