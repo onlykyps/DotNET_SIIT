b26cc25c4b0e3787273a0156271e9e319d9d0489
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-07-08 19:31:19 +0300

    linq

--- LinqLambda/Program.cs
+++ LinqLambda/Program.cs
@@ -31,7 +31,61 @@ namespace LinqLambda
 
          XDocument xDocument = XDocument.Load("Customers.xml");
 
+         XElement root = xDocument.Root;
+         //Console.WriteLine(root);
 
+         IEnumerable<XElement> descendants = root.Descendants();
+         Console.WriteLine(descendants.Count());
+
+         Console.WriteLine("--------------- Lista nume noduri -----------------");
+         IEnumerable<XName> johnSql = 
+            from xElem in descendants
+            select xElem.Name;
+
+         //XName MetSelect (XElement xElem)
+         //{
+         //   return xElem.Name;
+         //}
+
+         IEnumerable<XName> paulSql = descendants.Select(x => x.Name);  
+
+         foreach (XName name in johnSql)
+         {
+            Console.WriteLine(name);
+         }
+
+         Console.WriteLine("--------------- Lista noduri customer -----------------");
+
+         IEnumerable<XElement> stapaniiNostri = descendants.Select(x => x)
+            .Where(y => y.Name == "Customer");
+
+         foreach (XElement item in stapaniiNostri)
+         {
+               Console.WriteLine(item);
+         }
+
+
+         Console.WriteLine("--------------- Hourly Fee -----------------");
+
+         IEnumerable<string> hourlyFeeNodeValues = descendants.Where(x => x.Name == "HourlyFee")
+                                                               .Select(y => y.Value);
+
+         double medie = hourlyFeeNodeValues.Average(m => int.Parse(m));
+         double valMax = hourlyFeeNodeValues.Max(vmx => int.Parse(vmx));
+         double valMin = hourlyFeeNodeValues.Min(vmn => int.Parse(vmn));
+
+         Console.WriteLine($"Media {medie}, Maxim {valMax}, Minim {valMin}");
+
+         Console.WriteLine("--------------- Ordine desc -----------------");
+
+         IEnumerable<XElement> listaProiecte = descendants.Select(x => x)
+                                                         .Where(y => y.Name == "Project")
+                                                         .OrderByDescending(z => z.Attribute("numsize").Value);
+
+         foreach (XElement item in listaProiecte)
+         {
+            Console.WriteLine(item);
+         }
 
       }
    }
