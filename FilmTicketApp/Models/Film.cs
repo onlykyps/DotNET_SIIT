e79b2f14af56fdc5825c66bad54312e47afc3c57
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2025-09-14 16:03:37 +0300

    new film properties

--- FilmTicketApp/Models/Film.cs
+++ FilmTicketApp/Models/Film.cs
@@ -5,34 +5,51 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilmTicketApp.Models
 {
-   public class Film: IEntityBase
-   {
-      [Key]
-      public int Id { get; set; }
+    public class Film : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
 
-      public string Name { get; set; }
-      public string Description { get; set; }
-      public string ImageURL { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //public string ImageURL { get; set; }
 
-      public DateTime StartDate { get; set; }
-      public DateTime EndDate { get; set; }
+        [Display(Name = "Poster Image URL")]
+        public string? PosterImageUrl { get; set; }
 
-      public double Price { get; set; }
-      //public Actor[] Actors { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
-
-      [ForeignKey("CinemaID")]
-      public Cinema Cinema { get; set; }
-      public int CinemaID { get; set; }
-
-      [ForeignKey("ProducerID")]
-      public Producer Producer { get; set; }
-      public int ProducerID { get; set; }
-
-      public FilmGenre FilmGenre { get; set; }
-
-      public List<ActorToFilm> FilmActors { get; set; }
+        public double Price { get; set; }
+        //public Actor[] Actors { get; set; }
 
 
-   }
+        [ForeignKey("CinemaID")]
+        public Cinema Cinema { get; set; }
+        public int CinemaID { get; set; }
+
+        [ForeignKey("ProducerID")]
+        public Producer Producer { get; set; }
+        public int ProducerID { get; set; }
+
+        public FilmGenre FilmGenre { get; set; }
+
+        public List<ActorToFilm> FilmActors { get; set; }
+
+        [Display(Name = "Duration (minutes)")]
+        public int DurationMinutes { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Cast")]
+        public string? Cast { get; set; }
+
+        [Display(Name = "Trailer URL")]
+        public string? TrailerUrl { get; set; }
+
+        [Display(Name = "Rating")]
+        public string Rating { get; set; } = string.Empty;
+
+    }
 }
